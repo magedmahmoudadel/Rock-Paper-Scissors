207bf5e5f6777ca57526764b69f7b90270cc37a1
Maged Mahmoud Adel <magedmahmoud94@outlook.com>
2022-04-01 23:28:59 +0200

    Create game() function to play the game and count the score, Modify playRound to be used to calculate score later

--- script.js
+++ script.js
@@ -15,18 +15,50 @@ function computerPlay(){
 function playRound(playerSelection,computerSelection){
     playerSelection=playerSelection.toLowerCase();
     if (playerSelection===computerSelection){
-        return "It's a tie!"
+        console.log("It's a tie!");
+        return 0;
     }else if (playerSelection==='rock'&&computerSelection==='paper'){
-        return 'You Lose! Paper beats Rock';
+        console.log('You Lose! Paper beats Rock');
+        return 0;
     }else if (playerSelection==='rock'&&computerSelection==='scissors'){
-        return 'You Win! Rock beats Scissors';
+        console.log('You Win! Rock beats Scissors');
+        return 1;
     }else if (playerSelection==='paper'&&computerSelection==='rock'){
-        return 'You Win! Paper beats Rock';
+        console.log('You Win! Paper beats Rock');
+        return 1;
     }else if (playerSelection==='paper'&&computerSelection==='scissors'){
-        return 'You Lose! Scissors beats Paper';
+        console.log('You Lose! Scissors beats Paper');
+        return 0;
     }else if (playerSelection==='scissors'&&computerSelection==='paper'){
-        return 'You Win! Scissors beats Paper';
+        console.log('You Win! Scissors beats Paper');
+        return 1;
     }else if (playerSelection==='scissors'&&computerSelection==='rock'){
-        return 'You Lose! Rock beats Scissors';
+        console.log('You Lose! Rock beats Scissors');
+        return 0;
+    }else{
+        console.log('Invalid input');
     }
 }
+
+function game(){
+    let score = 0;
+    for(let i=0;i<5;i++){
+        const player=prompt("Rock, Paper, Scissors");
+        const computer = computerPlay();
+        let play = playRound(player,computer);
+        if(play){
+            score+=1;
+            console.log(score);
+        }else{
+            console.log(score);
+        }
+    }
+    if (score>=3){
+        console.log('You won the game!')
+    }else{
+        console.log('You lost. Better luck next time!')
+    }
+    
+}
+
+game();
